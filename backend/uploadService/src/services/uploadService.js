cb8eae3f7d915080aeecb41d0c870f75622d0950
Hong Quang <44489511+cungquang@users.noreply.github.com>
2024-02-19 12:00:28 -0800

    Delete console.log()

--- backend/uploadService/src/services/uploadService.js
+++ backend/uploadService/src/services/uploadService.js
@@ -44,7 +44,6 @@ class UploadService {
             return new Promise((resolve, reject) => {
                 //Success deliver the message
                 blobStreamWrite.on('finish', () =>{
-                    console.log(blobInstance.name);
                     resolve(blobInstance.name);
                 });
 
