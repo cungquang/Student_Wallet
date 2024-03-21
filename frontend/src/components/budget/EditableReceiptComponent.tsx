00447114b256620e1337e6f7d6fa1cf0bb3634fc
Hong Quang <44489511+cungquang@users.noreply.github.com>
2024-03-20 22:08:09 -0700

    update EditableReceiptComponent

--- frontend/src/components/budget/EditableReceiptComponent.tsx
+++ frontend/src/components/budget/EditableReceiptComponent.tsx
@@ -1,21 +1,77 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState, useRef } from 'react';
 import EditTextBoxComponent from './EditableComponent';
 
 //Style
 const BoxStyle = { width: '50%', height: '20px' };
-const ListStyle = { width: '50%', height: '60px' };
+const ListStyle = { width: '50%', height: '180px' };
 
 interface EditableReceiptComponentsProps {
     toEnable: boolean;
 };
 
-const EditableReceiptComponent: React.FC<EditableReceiptComponentsProps> = ({ toEnable }) => {
 
+function combineItems(data: Record<string, any>, prefix = 'item', maxItems = Infinity) {
+    const items = [];
+
+    for (let i = 0; i < maxItems; i++) {
+        const propertyName = `${prefix}${i}`;
+        if (propertyName in data) {
+            const item = JSON.parse(data[propertyName]);
+            items.push(item);
+        } else {
+            break;
+        }
+    }
+
+    return JSON.stringify(items);
+}
+
+
+const EditableReceiptComponent: React.FC<EditableReceiptComponentsProps> = ({ toEnable }) => {
+    const [byUser, setByUser] = useState("");
+    const [byDate, setByDate] = useState("");
+    const [byMerchant, setByMerchant] = useState("");
     const [totalCost, setTotalCost] = useState("");
     const [totalTax, setTotalTax] = useState("");
     const [listOfItems, setListOfItems] = useState("");
+    const newReceiptRef = useRef<any>(null);
+
+    //File has been uploaded & read - useEffect prevent re-render
+    useEffect(() => {
+        if (toEnable) {
+            const recentReceiptJSON = localStorage.getItem('recent_receipt');
+            if (recentReceiptJSON !== null) {
+                const newReceipt = JSON.parse(recentReceiptJSON);
+
+                //Fix data
+                newReceiptRef.current = newReceipt; 
+                const receiptData = newReceipt.receiptData; 
+                
+                //Set data
+                setByUser(newReceipt.userId);
+                
+                if (receiptData) {
+                    const newList = combineItems(receiptData);
+                    
+                    setByDate(receiptData.TransactionDate);
+                    setByMerchant(receiptData.MerchantName);
+                    setTotalCost(receiptData.Total);
+                    setTotalTax(receiptData.Tax);
+                    setListOfItems(newList);
+                }
+            }
+        }
+    }, [toEnable]);
 
     //Get data from frontend
+    const handleTransactionDate = (event: ChangeEvent<HTMLInputElement>) => {
+        setByDate(event.target.value);
+    };
+
+    const handleMerchantName = (event: ChangeEvent<HTMLInputElement>) => {
+        setByMerchant(event.target.value);
+    };
+
     const handleTotalCost = (event: ChangeEvent<HTMLInputElement>) => {
         setTotalCost(event.target.value);
     };
@@ -32,6 +88,24 @@ const EditableReceiptComponent: React.FC<EditableReceiptComponentsProps> = ({ to
         <div>
             {toEnable && (
                 <form style={{ width: '100%' }}>
+                    <div>User: {byUser}</div>
+                    <br/>
+                    <EditTextBoxComponent
+                        onChange={handleTransactionDate}
+                        textBoxId='TransactionDate'
+                        textBoxLabel="Transaction Date"
+                        textValue={byDate}
+                        styles={BoxStyle}
+                    />
+
+                    <EditTextBoxComponent
+                        onChange={handleMerchantName}
+                        textBoxId='MerchantName'
+                        textBoxLabel="Merchant Name"
+                        textValue={byMerchant}
+                        styles={BoxStyle}
+                    />
+
                     <EditTextBoxComponent
                         onChange={handleTotalCost}
                         textBoxId='TotalCost'
