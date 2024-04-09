9a7d0446d02fd5138d52c63320052073a9368ce2
scott0929 <scott0929@gmail.com>
2024-04-09 11:38:05 -0700

    aiService

--- backend/aiService/db.js
+++ backend/aiService/db.js
@@ -1,14 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
-const url =`mongodb://mongodb372:27017`
-const dbName = 'Unikeep';
+const url ="mongodb://mongoDB372:27017"
+const dbName = 'asn-db';
 
 let _db;
 
 async function dbConnection(){
     try{
         const client = await MongoClient.connect(url); 
-        console.log('Connected to MongoDB');
         _db = client.db(dbName);
+        console.log('Connected to MongoDB');
     } catch(error){
         console.error('Error while connecting to MongoDB: ', error);
         throw error;
@@ -17,6 +17,7 @@ async function dbConnection(){
 
 function getDB(){
     if(!_db){
+        console.log('DB not connected');
         throw new Error('DB not connected');
     }
     return _db;
